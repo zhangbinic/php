07ab146b2dca3195e8d2827a4e8bd33fc940ee66
zhangbinic <308964132@qq.com>
2015-02-09 11:43:58 +0800

    更改腐蚀机理识别的字体大小

--- python/index.py
+++ python/index.py
@@ -1,7 +1,25 @@
 # print 'Hello,World'
-for i in range(1,10):
-	for j in range(1,10):
-		#print j,'x',i,'=',j*i,'\t'
-		print format(j*i,'3')
-	print
-#print '\n'
+def print_99():
+	for i in range(1,10):
+		for j in range(1,10):
+			#print j,'x',i,'=',j*i,'\t'
+			print format(j*i,'3')
+		print
+	#print '\n'
+# print_99();
+import math
+#dir(math)
+#help(math.log10)
+#http://163.32.98.15/teacher/benme/python/Math.htm
+#print math.e
+
+def print_hello():
+	print 'Hello World'
+
+#print_hello();
+
+def print_log10(num):
+	print math.log10(num)
+
+#print_log10(1)
+
