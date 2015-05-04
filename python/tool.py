8fe8a30e0ead309ca8cf8e25f95dc29d227f15b5
zhangbinic <308964132@qq.com>
2015-05-04 10:35:25 +0800

    python访问应用程序的模块

---
+++ python/tool.py
@@ -0,0 +1,67 @@
+#coding=utf-8
+
+#'''启动应用程序'''
+
+import os
+
+def sqlserver():
+	#'''启动SQLSERVER客户端'''
+	os.startfile(r'C:\Program Files (x86)\Navicat for SQL Server\navicat.exe')
+
+def qq():
+	os.startfile(r'C:\Program Files (x86)\Tencent\QQ\QQProtect\Bin\QQProtect.exe')	
+
+def foxmail():
+	os.startfile(r'D:\Program Files\Foxmail 7.1\Foxmail.exe')
+
+def navicat():
+	os.startfile(r'C:\Program Files (x86)\Navicat for MySQL\navicat.exe')
+
+def td():
+	os.startfile(r'C:\Program Files (x86)\Common Files\Mercury Interactive\TD2000\TDExplorer.exe')
+
+def wamp():
+	os.startfile(r'D:\wamp\wampmanager.exe')
+
+def sublimetext2():
+	os.startfile(r'E:\Program Files (x86)\Sublime Text\SublimeText.exe')
+
+def sublimetext3():
+	os.startfile(r"E:\Program Files (x86)\Sublime Text\SublimeText.exe")
+
+def fiddle():
+	os.startfile(r"C:\Program Files (x86)\Fiddler2\Fiddler.exe")
+
+def phpstorm7():
+	os.startfile(r"C:\Program Files (x86)\JetBrains\PhpStorm 7.1\bin\PhpStorm.exe")
+
+def kugou():
+	os.startfile(r"E:\Program Files (x86)\KuGou\KGMusic\KuGou.exe")
+
+def feiq():
+	os.startfile(r'E:\app\FeiQ.exe')
+
+def ie():
+	os.startfile(r"C:\Program Files (x86)\Internet Explorer\iexplore.exe")
+
+def fireworks():
+	os.startfile(r"D:\应用软件\Fireworks 8\Fireworks.exe")
+
+def dw6():
+	os.startfile(r"C:\Program Files (x86)\Adobe\Adobe Dreamweaver CS6\Dreamweaver.exe")
+
+def em():
+	os.startfile(r"C:\Program Files (x86)\Netease\NetEase EIM\Start.exe")
+
+def mysqltable():
+	os.startfile(r'D:\应用软件\SQLyog10.2\SQLyog.exe')
+
+def chrome():
+	os.startfile(r'C:\Users\dell\AppData\Local\Google\Chrome\Application\chrome.exe')
+
+def wangpan():
+	os.startfile(r"C:\Program Files (x86)\360\360WangPan\360WangPan.exe")
+
+def se360():
+	os.startfile(r"E:\Program Files\360se6\Application\360se.exe")
+
