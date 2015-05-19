6c81ef4549f3af2c8529f2e5bea1c711e8d53344
zhangbinic <308964132@qq.com>
2015-05-19 17:48:11 +0800

    为什么读取邮件列表的时候自动过滤掉了第一个

--- python/jd.py
+++ python/jd.py
@@ -3,7 +3,7 @@
 def find_sender():
 	import fileinput,re
 	pat = re.compile('From (.*) <.*?>$')
-	a = ['From zhangbin <zhangbin@162.com>','From Yang <Yang@qq.com>']
+	a = ['From zhangbin <zhangbin@126.com>','From Yang <Yang@qq.com>']
 	# for line in fileinput.input(): 读取文件的暂时略过
 	for line in a:
 		m = pat.match(line)
@@ -12,4 +12,37 @@ def find_sender():
 			print m.group(1)
 
 # 查找指定字符串的指定值
-find_sender();
+# find_sender();
+
+def get_email():
+	import re
+	pat = re.compile(r'[a-z\-\.]+@[a-z\-\.]+',re.IGNORECASE)
+	addresses = set()
+	a = ['From zhangbin <zhangbin@126.com>','From Yang <Yang@qq.com>']
+	for line in a:
+		for address in pat.findall(line):
+			# print address
+			# 不知道怎么就过滤了第一个邮件地址
+			addresses.add(address)
+	# print addresses
+	for address in sorted(addresses):
+		print address
+
+get_email()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
