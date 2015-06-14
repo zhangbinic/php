558fa14689c84be146e983552c7191bbeae2558b
zhangbinic <308964132@qq.com>
2015-06-14 20:19:45 +0800

    整理笔记

--- onethink/Application/Common/Conf/config.php
+++ onethink/Application/Common/Conf/config.php
@@ -36,11 +36,11 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_HOST'   => 'localhost', // 服务器地址
     'DB_NAME'   => 'onethink', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'bhxz',  // 密码
-    'DB_PORT'   => '3310', // 端口
+    'DB_PWD'    => '111111',  // 密码
+    'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
